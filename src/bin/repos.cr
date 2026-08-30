4f3102d72d5d636069d3f9ec11c773d49560ea92
Anatoly Chernov <chertoly@gmail.com>
2026-08-30 17:38:59 +0300

    bundle exec rake

---
+++ src/bin/repos.cr
@@ -0,0 +1,17 @@
+require "./repos/*"
+
+VERSION = "0.0.0"
+
+case ARGV.size
+when 1
+  case ARGV[0]
+  when "-v", "version", "--version"
+    puts VERSION
+    exit
+  when "-h", "help", "--help"
+    print_help
+    exit
+  end
+end
+
+puts "repos is made with Crystal."
---
+++ src/bin/repos/print_help.cr
@@ -0,0 +1,7 @@
+HELP_MESSAGE = <<-S
+repos is a CLI made with Crystal.
+S
+
+def print_help
+  puts HELP_MESSAGE
+end
